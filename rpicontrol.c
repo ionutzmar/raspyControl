2d53534af2a27c30d928940cce9d20bf72652e2f
Marius <ionutzmar@gmail.com>
2016-05-18 18:02:17 +0000

    server updated

--- rpicontrol.c
+++ rpicontrol.c
@@ -10,7 +10,18 @@
 int sockfd, newsockfd;
 struct sockaddr_in serv_addr, cli_addr;
 const int portno = 8887;
+int pins[] = {8, 9, 7, 0, 2, 3, 12, 13, 14, 21, 22, 23, 24, 25, 15,  16, 1, 4, 5, 6, 10, 11, 26, 27, 28, 29};
 
+void resetPins()
+{
+    int i;
+    for(i = 0; i < 26; i++)
+    {
+	pinMode(pins[i], 0);
+	digitalWrite(pins[i], 0);
+    }
+
+}
 void error(const char *msg)
 {
     perror(msg);
@@ -49,9 +60,11 @@ int main(int argc, char *argv[])
 {
     wiringPiSetup();
     createSocket();
+reconnect:
     acceptClient();
-
+    resetPins();
     char buffer[4];
+    char bufferOut[4];
     bzero(buffer,4);
     while(read(newsockfd,  buffer, 4) > 0)
     {
@@ -62,6 +75,17 @@ int main(int argc, char *argv[])
         //    close(sockfd);
         //    return 0;
         //}
+	pinMode(buffer[0], buffer[1]);
+        if(buffer[1])
+            digitalWrite(buffer[0], buffer[2]);
+	else
+	{
+	    bzero(bufferOut, 4);
+	    bufferOut[0] = buffer[0];
+	    bufferOut[2] = digitalRead(buffer[0]);
+	    bufferOut[3] = 102;
+	    write(newsockfd, bufferOut, 4);
+	}
         int i;
         for(i = 0; i < 4 ; i++)
         {
@@ -70,8 +94,9 @@ int main(int argc, char *argv[])
 	}
 	printf("\n");
     }
-    error("ERROR reading from socket");
+    printf("Client disconnected! Waiting for a new one...\n");
     close(newsockfd);
+goto reconnect;
     close(sockfd);
     return 0;
 }
